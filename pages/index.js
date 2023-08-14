5821ae14eb44892f655aaf74caea4e2452bd46b9
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-14 14:06:22 +0200

    modified code

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,19 @@ const URL = "https://example-apis.vercel.app/api/art";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function HomePage() {
-  const { data: artPieces } = useSWR(URL, fetcher);
+  const { data: artPieces, error, isLoading } = useSWR(URL, fetcher);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Failed to Load...</div>;
+  }
 
   return (
     <div>
       <h1>Art Gallery</h1>
-      {artPieces ? <ArtPieces pieces={artPieces} /> : <p>Loading...</p>}
+      <ArtPieces pieces={artPieces} />
     </div>
   );
 }
