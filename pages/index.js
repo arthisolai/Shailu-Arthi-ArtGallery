a5e7721ad64685f9e6561f5c63f0efbbba57632b
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-08-14 14:34:51 +0200

    user-story2 in progress

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import { Spotlight } from "../Spotlight";
+
 export default function HomePage() {
+  const [spotlightPiece, setSpotlightPiece] = useState(null);
+  function getRandomArtPiece(artPieces) {
+    const randomIndex = Math.floor(Math.random() * artPieces.length);
+    return artPieces[randomIndex];
+  }
+
+  useEffect(() => {
+    if (artPieces) {
+      setSpotlightPiece(getRandomArtPiece(artPieces));
+    }
+  }, []);
+
+  if (error) return <div>Error loading art pieces</div>;
+  if (!artPieces) return <div>Loading...</div>;
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <Spotlight image={artPieces.imageSource} artist={artPieces.artist} />
     </div>
   );
 }
