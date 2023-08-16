244823caabb3ce92fb4e4cb75830ee0bccaf506d
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-16 13:04:47 +0200

    some minor changes

--- Components/CommentForm/CommentForm.js
+++ Components/CommentForm/CommentForm.js
@@ -10,15 +10,18 @@ export default function CommentForm({ onSubmitComment }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="comment-form">
       <input
         type="text"
         value={commentText}
         onChange={(e) => setCommentText(e.target.value)}
         placeholder="Write a comment..."
         required
+        className="comment-input"
       />
-      <button type="submit">Send</button>
+      <button type="submit" className="comment-submit-button">
+        Send
+      </button>
     </form>
   );
 }
